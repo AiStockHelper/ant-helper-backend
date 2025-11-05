e9cf24c73affc5e4fb7ec3c6d4ff1e86c07412c0
yooooonshine <hl3470@naver.com>
2025-11-05 21:55:00 +0900

    fix: ApiException에 message를 넘겨도 못받는 에러 수정

--- backend/src/main/java/com/backend/common/exception/ApiException.java
+++ backend/src/main/java/com/backend/common/exception/ApiException.java
@@ -8,6 +8,11 @@ public class ApiException extends RuntimeException {
 	private final ErrorCode errorCode;
 
 	public ApiException(ErrorCode errorCode, String message) {
+		super(message);
+		this.errorCode = errorCode;
+	}
+
+	public ApiException(ErrorCode errorCode) {
 		super(errorCode.getMessage());
 		this.errorCode = errorCode;
 	}
